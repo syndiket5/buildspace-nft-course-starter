f39dd0268f2901d8e15739d777e2ec286b034394
syndiket5 <61816916+syndiket5@users.noreply.github.com>
2022-02-05 20:08:40 +0700

    almost final

--- src/App.js
+++ src/App.js
@@ -9,9 +9,11 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
+const rinkebyChainId = "0x4"; 
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
+  const [currentState, setCurrentState] = useState("ready");
 
   const checkIfWalletIsConnected = async () => {
     const { ethereum } = window;
@@ -25,10 +27,20 @@ const App = () => {
 
     const accounts = await ethereum.request({ method: 'eth_accounts' });
 
+    let chainId = await ethereum.request({ method: 'eth_chainId' });
+    console.log("Connected to chain " + chainId);
+
+    
+    if (chainId !== rinkebyChainId) {
+      alert("You are not connected to the Rinkeby Test Network!");
+    }
+
     if (accounts.length !== 0){
       const account = accounts[0];
       console.log("Found an authorized account:", account);
       setCurrentAccount(account);
+
+      setupEventListener();
     }else{
       console.log("No authorized account found");
     }
@@ -44,16 +56,53 @@ const App = () => {
       }
 
       const accounts = await ethereum.request({ method: "eth_requestAccounts"});
+
+      let chainId = await ethereum.request({ method: 'eth_chainId' });
+      console.log("Connected to chain " + chainId);
+
+      // String, hex code of the chainId of the Rinkebey test network
+      const rinkebyChainId = "0x4"; 
+      if (chainId !== rinkebyChainId) {
+      alert("You are not connected to the Rinkeby Test Network!");
+}
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
-
+      setupEventListener();
     }catch(error){
       console.log(error);
     }
   }
 
+
+
+  const setupEventListener = async () => {
+    
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+  
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          alert(`Hey there, We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+        });
+
+        console.log("Setup event listener!")
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   const askContractToMintNft = async () => {
-    const CONTRACT_ADDRESS = "0x7Bb4Ef3c516104f653E21B663Cbdd782874D7287";
+    const CONTRACT_ADDRESS = "0x13b7DEf65C10617a6Cb5d966EEB2fE6CD3aB3164";
     try{
       const { ethereum } = window;
 
@@ -66,7 +115,9 @@ const App = () => {
         let nftTxn = await connectedContract.makeAnEpicNFT();
 
         console.log("Mining... please wait!");
+        setCurrentState("notReady");
         await nftTxn.wait();
+        setCurrentState("ready");
 
         console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
       } else {
@@ -84,6 +135,18 @@ const App = () => {
     </button>
   );
 
+  const renderMintUI = () => {
+    <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
+      Mint NFT
+    </button>
+  }
+
+  const renderWaitUI = () => {
+    <button className="cta-button connect-wallet-button" disabled>
+      Please Wait!
+    </button>
+  }
+
   useEffect(() => {
     checkIfWalletIsConnected();
   }, [])
@@ -96,13 +159,12 @@ const App = () => {
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
           </p>
-          {currentAccount === "" ? (
-            renderNotConnectedContainer()
-          ) : (
-            <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
-              Mint NFT
-            </button>
+          {currentAccount === "" ? renderNotConnectedContainer() : (
+          <>
+            { currentState === "ready" ? renderMintUI() : renderWaitUI() }
+          </>
           )}
+          
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
